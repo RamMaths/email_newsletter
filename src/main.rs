0d225ad0a90b586dca48f3045af5b0abb35e80be
RamMaths <ram.hdzven@gmail.com>
2024-02-09 23:03:40 -0600

    succesfully connected to database in integrated test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use email_newsletter::startup::run;
+use email_newsletter::{
+    startup::run,
+    configuration::get_configuration
+};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind the address");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(&address).expect("Failed to bind the address");
     run(listener)?.await
 }
