4134b457940fc46d4bcdd63c2df7ee90c0ff7d6e
Ramses <ram.hdzven@gmail.com>
2024-04-05 17:17:19 -0600

    refactoring our test suite

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,25 @@
 use email_newsletter::{
-    startup::run,
     configuration::get_configuration,
     telemetry::*,
-    email_client::EmailClient
+    startup::*
 };
-use std::net::TcpListener;
-use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    //setting the subscriber (telemetry)
     let subscriber = get_subscriber(
         "email_newsletter".to_string(),
         "info".to_string(),
         std::io::stdout
     );
     init_subscriber(subscriber);
-    let configuration = get_configuration().expect("Failed to read configuration");
 
-    let address = format!("{}:{}", &configuration.application.host, &configuration.application.port);
-    let listener = TcpListener::bind(&address).expect("Failed to bind the address");
+    let configuration = get_configuration()
+        .expect("Failed to load configuration file");
 
-    //Database
-    let db_pool = PgPoolOptions::new()
-        .connect_lazy_with(configuration.database.with_db());
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
 
-    //Building an email client
-    let email_sender = configuration.email_client.sender().expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url.clone(),
-        email_sender,
-        configuration.email_client.authorization_token,
-        timeout
-    );
-
-
-    println!("Application running on {}:{}", &configuration.application.host, &configuration.application.port);
-    //
-    run(listener, db_pool, email_client)?.await
+    Ok(())
 }
+
--- src/startup.rs
+++ src/startup.rs
@@ -10,24 +10,82 @@ use super::routes::health_check;
 use super::routes::subscribe;
 use super::email_client::EmailClient;
 use tracing_actix_web::TracingLogger;
+use super::configuration::{
+    Settings,
+    DatabaseSettings
+};
+use sqlx::postgres::PgPoolOptions;
 
-pub fn run(
-    listener: TcpListener,
-    connection: PgPool,
-    email_client: EmailClient
-    ) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(connection);
-    let email_client = web::Data::new(email_client);
-    let server = HttpServer::new(move || {
-        App::new()
-            .wrap(TracingLogger::default())
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
-            .app_data(email_client.clone())
-    })
-    .listen(listener)?
-    .run();
+pub struct Application {
+    port: u16,
+    server: Server
+}
 
-    Ok(server)
+impl Application {
+    fn run(
+        listener: TcpListener,
+        connection: PgPool,
+        email_client: EmailClient
+        ) -> Result<Server, std::io::Error> {
+
+        let connection = web::Data::new(connection);
+        let email_client = web::Data::new(email_client);
+        let server = HttpServer::new(move || {
+            App::new()
+                .wrap(TracingLogger::default())
+                .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
+                .app_data(connection.clone())
+                .app_data(email_client.clone())
+        })
+        .listen(listener)?
+        .run();
+
+        Ok(server)
+    }
+
+    pub async fn build(configuration: Settings) -> Result<Application, std::io::Error> {
+        //Database
+        let db_pool = get_connection_pool(&configuration.database);
+
+        //Building an email client
+        let email_sender = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address");
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url.clone(),
+            email_sender,
+            configuration.email_client.authorization_token,
+            timeout
+        );
+
+        let address = format!(
+            "{}:{}",
+            &configuration.application.host,
+            &configuration.application.port
+        );
+        let listener = TcpListener::bind(&address).expect("Failed to bind the address");
+
+        let port = listener.local_addr().unwrap().port();
+        let server = Application::run(listener, db_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+
+pub fn get_connection_pool (
+    configuration: &DatabaseSettings
+) -> PgPool {
+    PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
